acfe36c07137b0dd429787a525906662ea454622
Arielle <celestialdesolation7@gmail.com>
2026-02-08 18:44:04 +0800

    实现echo服务器

--- src/client.cpp
+++ src/client.cpp
@@ -1,18 +1,19 @@
+#include "util.h"
 #include <arpa/inet.h>
 #include <cstdio>
 #include <cstring>
 #include <iostream>
 #include <netinet/in.h>
+#include <ostream>
+#include <strings.h>
 #include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 int main() {
   // 初始化本机socket
   int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd == -1) {
-    perror("[客户端] socket创建失败");
-    return -1;
-  }
+  errif(sockfd == -1, "[客户端] socket创建失败");
 
   // 初始化服务器地址
   struct sockaddr_in server_addr;
@@ -22,13 +23,35 @@ int main() {
   server_addr.sin_port = htons(8888);
   // 对应server那边的地址
 
-  if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
-      -1) {
-    perror("[客户端] 与server建立连接时失败");
-    return -1;
-  }
+  errif(connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) ==
+            -1,
+        "[客户端] 与server建立连接时失败\n");
 
-  std::cout << "[客户端] 成功与server建立TCP连接" << std::endl;
+  std::cout << "[客户端] 成功与server建立TCP连接\n";
+
+  while (true) {
+    char buf[1024];
+    bzero(buf, sizeof(buf));
+    scanf("%s", buf);
+    ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+
+    if (write_bytes == -1) {
+      std::cout << "[客户端] socket连接已经关闭，无法再写入数据\n";
+      break;
+    }
+
+    bzero(buf, sizeof(buf));
+    ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+    if (read_bytes > 0) {
+      std::cout << "[客户端] 收到来自服务器的数据：\n" << buf << std::endl;
+    } else if (read_bytes == 0) {
+      std::cout << "[客户端] 服务器 socket 断开连接\n";
+      break;
+    } else if (read_bytes == -1) {
+      close(sockfd);
+      errif(true, "[客户端] socket 读取失败");
+    }
+  }
 
   close(sockfd);
   return 0;
