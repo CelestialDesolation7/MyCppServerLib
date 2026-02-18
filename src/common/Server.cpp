25a4050f523f4565ba54752e67a59197dcca93f3
CelestialDesolation7 <137397992+CelestialDesolation7@users.noreply.github.com>
2026-02-18 16:18:17 +0800

    day12-主从Reactor

--- src/common/Server.cpp
+++ src/common/Server.cpp
@@ -2,6 +2,7 @@
 #include "Acceptor.h"
 #include "Buffer.h"
 #include "Connection.h"
+#include "EventLoop.h"
 #include "InetAddress.h"
 #include "Socket.h"
 #include "ThreadPool.h"
@@ -17,49 +18,63 @@
 
 #define READ_BUFFER 1024
 
-Server::Server(Eventloop *_loop) : loop(_loop) {
+Server::Server(Eventloop *_loop) : mainReactor(_loop) {
     // 现在资源初始化交由 Acceptor
-    acceptor = new Acceptor(loop);
+    acceptor = new Acceptor(mainReactor);
     std::function<void(Socket *, InetAddress *)> cb =
         std::bind(&Server::newConnection, this, std::placeholders::_1, std::placeholders::_2);
     acceptor->setNewConnectionCallback(cb);
-    threadPool = new ThreadPool();
+
+    // 1. 初始化线程池
+    int threadNum = std::thread::hardware_concurrency();
+    threadPool = new ThreadPool(threadNum);
     std::cout << "[server] ThreadPool initialized" << std::endl;
+
+    for (int i = 0; i < threadNum; ++i) {
+        Eventloop *subReactor = new Eventloop();
+        subReactors.push_back(subReactor);
+        // loop 函数作为任务加入 threadPool
+        // 每个线程现在取走一个 loop 任务，陷入死循环，这就是 subReactor 的启动
+        threadPool->add(std::bind(&Eventloop::loop, subReactor));
+    }
+    std::cout << "[server] Main Reactor and " << threadNum << " Sub Reactors initialized"
+              << std::endl;
 }
 
 Server::~Server() {
     delete acceptor;
     delete threadPool;
+    for (Eventloop *loop : subReactors) {
+        delete loop;
+    }
 }
 
 // 这个函数就是以前 accept 之后的那部分逻辑
 void Server::newConnection(Socket *client_sock, InetAddress *client_addr) {
-    // 注意：clnt_addr 在这里打印完就可以删了，或者保存到 Connection 里（Day 8 先不保存）
     std::cout << "[server] new client fd" << client_sock->getFd()
               << "! IP:" << inet_ntoa(client_addr->addr.sin_addr) << " PortL "
               << ntohs(client_addr->addr.sin_port) << std::endl;
 
-    Connection *conn = new Connection(loop, client_sock);
+    int dispatchIdx = client_sock->getFd() % subReactors.size();
+    Eventloop *ioLoop = subReactors[dispatchIdx];
+
+    // day 12 修改：连接被绑定至 subReactor
+    Connection *conn = new Connection(ioLoop, client_sock);
 
     // 定义一个 lambda 表达式，捕获 this （Server类指针）拿到ThreadPool
     // 定义 Server socket 的业务逻辑：
     // 把业务函数写在这里，作为一个Task加入线程池
     std::function<void(Connection *)> msgCb = [this](Connection *conn) {
-        // 这里的 lambda 是为了捕获 this (Server) 拿到 threadPool
-        // 把具体的业务 (Echo) 作为一个 Task 添加到线程池
-        // 注意：这里有严重的线程不安全！c->readBuffer 在主线程写，子线程读
-        // 后面会修
-
-        threadPool->add([conn]() {
-            // std::this_thread::sleep_for(std::chrono::seconds(2));
-            std::string msg = conn->readBuffer()->retrieveAllAsString();
-            std::cout << "Thread " << std::this_thread::get_id() << " recieve: " << msg
-                      << std::endl;
-            conn->send(msg);
-        });
+        std::string msg = conn->readBuffer()->retrieveAllAsString();
+        std::cout << "[Thread " << std::this_thread::get_id() << "] recv: " << msg << std::endl;
+        conn->send(msg);
     };
+    //
     conn->setOnMessageCallback(msgCb);
 
+    // deleteConnection 会在子线程被调用，但 connections map 在主线程
+    // 这会导致竞态条件。
+    // 我们暂时不修复杂的跨线程安全问题，先跑通逻辑。
     std::function<void(Socket *)> deleteCb =
         std::bind(&Server::deleteConnection, this, std::placeholders::_1);
     conn->setDeleteConnectionCallback(deleteCb);
